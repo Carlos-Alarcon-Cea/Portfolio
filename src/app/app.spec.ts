20d1b99389ae1d5873814f0fd67b189ddef703b2
jalarcon5 <jalarcon@bitworks.com.sv>
2026-07-20 23:18:03 -0600

    Migrar imagenes, archivos

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -18,6 +18,6 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     await fixture.whenStable();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Base visual del portafolio');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Navbar y assets listos');
   });
 });
